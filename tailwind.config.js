6d91e95aac5dff580d0bfd660beabbc8a545ecbc
Ryan <jesselockard99@gmail.com>
2021-03-21 00:26:37 -0400

    added additional purge configuration and options

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 module.exports = {
-  purge: [
-    './src/html/**/*.html',
-    './src/html/**/*.js',
-  ],
+  // See https://tailwindcss.com/docs/optimizing-for-production#overview
+  // for more options on optimization.
+  purge: {
+    // Remove all styling; leaves a minimal amount of accessibility-centered styling.
+    // mode: 'all',
+    // preserveHtmlElements: false,
+    content: [
+      './src/html/**/*.html',
+      './src/html/**/*.js',
+    ],
+    // Remove unused animation keyframes
+    // options: {
+    //   keyframes: true
+    // }
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
