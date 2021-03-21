b6f8fea70f0e49888f8dbc40d36eb91e87ad361f
Ryan <jesselockard99@gmail.com>
2021-03-21 00:49:18 -0400

    added example commands

--- src/client/client.ts
+++ src/client/client.ts
@@ -6,3 +6,16 @@ onNet('helloclient', message => {
   console.log(`The server said: ${message}`);
 });
 
+onNet('_boilerplate:showNui', () => {
+  console.log('got it')
+  SendNuiMessage(JSON.stringify({
+    show: true
+  }))
+})
+
+onNet('_boilerplate:hideNui', () => {
+  console.log('got it')
+  SendNuiMessage(JSON.stringify({
+    show: false
+  }))
+})
--- src/server/server.ts
+++ src/server/server.ts
@@ -5,3 +5,19 @@ onNet('helloserver', () => {
 
   emitNet('helloclient', _source, 'i got your message!');
 });
+
+RegisterCommand(
+  'showbanner',
+  (source, args, raw) => {
+    emitNet('_boilerplate:showNui', source)
+  },
+  false
+)
+
+RegisterCommand(
+  'hidebanner',
+  (source, args, raw) => {
+    emitNet('_boilerplate:hideNui', source)
+  },
+  false
+)
