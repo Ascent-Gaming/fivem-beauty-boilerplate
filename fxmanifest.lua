107bce4d8e222973c495f95f7ae616422c3789a3
Ryan <jesselockard99@gmail.com>
2021-03-21 02:11:41 -0400

    added only styles/dist to FXManifest client_scripts

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,7 +10,7 @@ client_script 'dist/client/*.client.js'
 server_script 'dist/server/*.server.js'
 ui_page 'src/html/index.html'
 files {
-  'src/html/**/*.css',
+  'src/html/**/styles/dist/*.css',
   'src/html/**/*.js',
   'src/html/**/*.html'
 }
